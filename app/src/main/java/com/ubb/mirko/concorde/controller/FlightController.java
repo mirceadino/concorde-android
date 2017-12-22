656d2ad062eb14641029ad0b6fbd45c3c7bb9270
mirko <mirceadino97@gmail.com>
2017-12-22 22:46:43 +0200

    Remove populater and cleaner

--- app/src/main/java/com/ubb/mirko/concorde/controller/FlightController.java
+++ app/src/main/java/com/ubb/mirko/concorde/controller/FlightController.java
@@ -1,12 +1,10 @@
 package com.ubb.mirko.concorde.controller;
 
-import com.ubb.mirko.concorde.db.AppDatabase;
 import com.ubb.mirko.concorde.model.Flight;
 import com.ubb.mirko.concorde.repository.FlightRepository;
 import com.ubb.mirko.concorde.repository.FlightRepositoryWithRoom;
 
 import java.util.List;
-import java.util.Random;
 
 /**
  * Created by mirko on 16/12/2017.
@@ -22,8 +20,6 @@ public class FlightController {
 
     public FlightController(FlightRepository repository) {
         repository_ = repository;
-        // cleanDb();
-        // populateDb();
     }
 
     public List<Flight> getAllFlights() {
@@ -36,35 +32,5 @@ public class FlightController {
 
     public void removeFlight(Flight deletedFlight) {
         repository_.remove(deletedFlight);
-    }
-
-    private void cleanDb() {
-        List<Flight> flights = getAllFlights();
-        for (Flight f : flights) {
-            removeFlight(f);
-        }
-    }
-
-    private int getRandom(Random random, int min, int max) {
-        return random.nextInt(max - min + 1) + min;
-    }
-
-    private void populateDb() {
-        Random random = new Random();
-        Flight[] flights = new Flight[4];
-        flights[0] = new Flight("Bucharest", "Budapest", getRandom(random, 10, 50));
-        flights[1] = new Flight("Paris", "Budapest", getRandom(random, 10, 50));
-        flights[2] = new Flight("Paris", "London", getRandom(random, 10, 50));
-        flights[3] = new Flight("London", "Paris", getRandom(random, 10, 50));
-
-        for (int i = 0; i < 20; ++i) {
-            for (int j = 0; j < flights.length; ++j) {
-                flights[j].setPrice(getRandom(random, 10, 50));
-            }
-        }
-
-        for (int i = 0; i < flights.length; ++i) {
-            addFlight(flights[i]);
-        }
     }
 }
